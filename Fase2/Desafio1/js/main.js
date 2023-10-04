fcdab3eb4360d261d6ca13474c3aec9e354575d1
NicoClemente <clementes.nicolas@gmail.com>
2023-10-04 18:41:51 -0300

    commit

--- Fase2/Desafio1/js/main.js
+++ Fase2/Desafio1/js/main.js
@@ -1,32 +1,47 @@
-let colores = ["red", "green", "blue", "black"];
-let colorInput = "";
+console.log(
+    "Escribe en la pantalla un color y apreta enter para agregarlo al array"
+  );
+  let colores = ["red", "green", "blue", "black"];
+  let colorInput = "";
 
-const mainElement = document.querySelector("main");
-const h2Element = document.querySelector("main > #color");
-const colorPrevButton = document.getElementsByTagName('button')[0];
-const colorNextButton = document.getElementsByTagName('button')[1];
-const addColorButton = document.getElementsByTagName('button')[2];
-let colorIndex = 0;
+  const mainElement = document.querySelector("main");
+  const h2Element = document.querySelector("main h2");
+  var buttons = document.getElementsByTagName('button');
+    const colorPrevButton = buttons[0];
+    const colorNextButton = buttons[1];
+  let colorIndex = 0;
 
-const cambiarColorFondo = () => {
+  const cambiarColorFondo = () => {
     mainElement.style.backgroundColor = colores[colorIndex];
-    h2Element.textContent = colores[colorIndex].charAt(0).toUpperCase() + colores[colorIndex].slice(1);
-};
+    h2Element.textContent =
+      colores[colorIndex].charAt(0).toUpperCase() +
+      colores[colorIndex].slice(1);
+  };
 
-colorPrevButton.addEventListener("click", () => {
+  colorPrevButton.addEventListener("click", () => {
     colorIndex = (colorIndex - 1 + colores.length) % colores.length;
     cambiarColorFondo();
-});
+  });
 
-colorNextButton.addEventListener("click", () => {
+  colorNextButton.addEventListener("click", () => {
     colorIndex = (colorIndex + 1) % colores.length;
     cambiarColorFondo();
-});
+  });
 
-addColorButton.addEventListener("click", () => {
-    const nuevoColor = prompt("Por favor, ingresa un nuevo color:");
-    if (nuevoColor) {
-        colores.push(nuevoColor.toLowerCase());
-        cambiarColorFondo();
+  document.addEventListener("keydown", (event) => {
+    const key = event.key.toLowerCase();
+    if (event.key !== "Enter") colorInput += key;
+  });
+
+  document.addEventListener("keyup", (event) => {
+    if (event.key === "Enter") {
+      const nuevosColores = colorInput.split(",");
+      console.log(nuevosColores);
+
+      colores = [...colores, ...nuevosColores];
+      colorInput = "";
+
+      cambiarColorFondo();
     }
-});
+   
+  });
